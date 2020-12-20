7e1d1bfbd7b06f1a19dd2f6f6d9e30504dec79f6
Marc Gurt <marcgurt.developer@gmail.com>
2020-12-20 08:54:53 +0100

    populate movies select

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import pec3 from './pac3/pec3';
 
+console.log(pec3);
+
 pec3.setMovieHeading(1,  ".movie__title", ".movie__info", ".movie__director");
+pec3.initMovieSelect("#select-movie");
--- src/pac3/pec3.js
+++ src/pac3/pec3.js
@@ -1,20 +1,40 @@
 import pec2 from './pec2';
 
 export async function setMovieHeading(movieId ,titleSelector, infoSelector, directorSelector) {
-     // TODO: Obtenemos los elementos del DOM con QuerySelector y los almacenamos en una variable
     let titleElement = document.querySelector(titleSelector);
     let infoElement = document.querySelector(infoSelector);
     let directorElement = document.querySelector(directorSelector);
 
-    console.log(titleElement, infoElement, directorElement);
+    let movieInfo = await pec2.getMovieInfo(movieId);
 
-     // TODO: Obtenemos la información de la película llamando al método de pec2.js
-     let movieInfo = await pec2.getMovieInfo(movieId);
-
-     console.log(movieInfo);
-    
-     // TODO: Sustituimos los datos utilizando un método de reemplazo como innerHTML
      titleElement.innerHTML = movieInfo.name;
      infoElement.innerHTML = `Episode ${movieInfo.episodeID} - ${movieInfo.release}`;
      directorElement.innerHTML = `Director: ${movieInfo.director}`;
-}    
+}
+
+export async function initMovieSelect(selector) {
+    let selectElement = document.querySelector(selector);
+
+    let movieList = await pec2.listMoviesSorted();
+    movieList.unshift({ name: 'Select a movie', episodeID: null });
+
+    console.log(movieList);
+
+    movieList.forEach(movie => {
+        addOptionToSelect(selectElement, movie.episodeID, movie.name);
+    });
+}
+
+function addOptionToSelect(selectElement, id, text){
+    var option = document.createElement("option");
+
+    option.value = id;
+    option.text = text;
+    
+    selectElement.add(option);
+}
+
+export default {
+    setMovieHeading,
+    initMovieSelect
+}
